a939565d0888aedeccbbba092e09f0143a037ecd
isaacismaelx14 <isaacismaelx14@gmail.com>
2020-03-18 19:54:43 -0400

    navbar and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//Routes
+import {HomePageComponent} from './componentes/home-page/home-page.component';
+import {LoginPageComponent} from './componentes/login-page/login-page.component';
+import {RegisterPageComponent} from './componentes/register-page/register-page.component';
+import {LogedPageComponent} from './componentes/loged-page/loged-page.component';
+import {NotFoundPageComponent} from  './componentes/not-found-page/not-found-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'loged', component: LogedPageComponent},
+  {path: '**', component: NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
